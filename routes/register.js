70535ece2b75581c3b2f717da5a60e2e28ec4354
Cutts, Charlotte <charlotte.cutts@bskyb.com>
2014-11-20 11:12:05 +0000

    not working, try it?

---
+++ routes/register.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.put('/register', function(req,res) {
+  console.log('asdfadsfadf');
+})
+//router.get('/register', function(req, res) {
+//  res.render('index', { title: 'registering' });
+//});
+
+
+
+module.exports = router;
