f4a0fc62f2470fecd12598d90ac1f8035cfecdd9
Cutts, Charlotte <charlotte.cutts@bskyb.com>
2014-12-04 17:15:19 +0000

    bits and pieces inc logout button, redirecting to mypeople

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ var user = '';
 
 ///* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Trading Places', user: req.session.user,  invalid: req.param("invalid")});
+  if (req.session.user){
+    res.redirect('/mypeople');
+  } else {
+    res.render('index', { title: 'Trading Places', user: req.session.user,  invalid: req.param("invalid")});
+  }
 });
 
 module.exports = router;
