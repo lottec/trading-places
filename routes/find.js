a317df41160f72fc0bdd3853b6c8b72edce8535f
Cutts, Charlotte <charlotte.cutts@bskyb.com>
2014-12-09 13:24:37 +0000

    find page now includes manager email as hidden tag

--- routes/find.js
+++ routes/find.js
@@ -5,10 +5,8 @@ var host = 'http://db.cistechfutures.net';
 var port = 8098;
 
 router.get('/', function(req,res) {
-    //res.render('find', { title: 'Find' });
     getAllTeamMembers(req, function(teamMembers) {
         res.render('find', { title: 'Find', team_members: teamMembers});
-        //res.send(teamMembers);
     });
 });
 
@@ -21,22 +19,20 @@ var getAllTeamMembers = function(req, callback) {
         .get(host + ":" + port + "/buckets/mt-add-team-member/keys?keys=true")
         .end(function(error, result){
             var keys = JSON.parse(result.text).keys;
-
             keys.forEach(function(key) {
                 request
                     .get(host + ":" + port + "/riak/mt-add-team-member/" + key)
                     .end(function(error, result){
-
                         try {
                             var json = JSON.parse(result.text);
                             json.data['key'] = key;
                             if (json.data.availability == true && json.data.manager != req.session.user.username) {
+                                json.data['manager_email'] = getManagerEmail(json.data.manager);
                                 teamMembers.push(json);
                             }
                         } catch (e) {
 
                         }
-
                         if (i == keys.length) {
                             callback(teamMembers);
                         }
@@ -46,5 +42,24 @@ var getAllTeamMembers = function(req, callback) {
         });
 };
 
+var getManagerEmail = function(key) {
+
+    request
+        .get(host + ":" + port + "/riak/mt-register/" + key)
+        .end(function(error, result) {
+            console.log(result);
+            var email = result.body.data.email;
+            return email;
+        });
+};
+
 module.exports = router;
 module.testExports = {getAllTeamMembers: getAllTeamMembers, request: request, host: host, port: port};
+
+//for jade updateModalFields
+//                var href = "mailto:' + manager.email +
+//                '?subject=' + subject +
+//                '&bcc=' + bcc +
+//                '&body=' + body +
+//
+//                $("mail").attr("href",href);
