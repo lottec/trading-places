099e0eb331cf178e49ef2a6bde30ba1e392a1d15
Cutts, Charlotte <charlotte.cutts@bskyb.com>
2014-12-15 16:17:26 +0000

    unfinished - continuing with event assembly

--- event_assembling/event_assembler.js
+++ event_assembling/event_assembler.js
@@ -13,7 +13,45 @@ var event = {"timestamp": 12345 ,
     }
 }
 
+receive event
+compile into object
+save in riak db
+
+
+
+
 var sendEvent = function(event, callback) {
     var eventJSON = {timestamp: Date.now(), data: event.data};
     riakWrite.write(event.event, eventJSON, callback);
 };
+
+
+var eventToObject = function(type, data) {
+    take first part of type name (eg user) and use as riak bucket name
+    take second part of type name (eg create/update/del) and translate to http verb
+
+
+
+    user-create
+    user-update
+    user-del
+    teammember-create
+    teammember-update
+    teammember-del
+
+    user-login
+    user-logout
+
+    var typeSplit = type.split('-');
+    var bucket = typeSplit[0];
+    var action = typeSplit[1];
+
+
+
+
+
+
+    riakWrite.write(bucket, data);
+
+
+}
